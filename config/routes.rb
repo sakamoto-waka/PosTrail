429cde0f1060c04598f7a579fa322551cb0b56c9
sakamoto-waka <wakanmhl@gmail.com>
2022-05-03 15:11:30 +0000

    [update] routing for pub/homes/top+about

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
+  
+  scope module: :public do
+    root 'homes#top'
+    get 'about' => 'homes#about'
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
