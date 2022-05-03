ea7d9f0cfeaf39b57fc853ad17a7f7b250893d45
sakamoto-waka <wakanmhl@gmail.com>
2022-05-03 15:24:14 +0000

    [update] adm/homes#top

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
     get 'about' => 'homes#about'
   end
   
+  namespace :admin do
+    root 'homes#top'
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
