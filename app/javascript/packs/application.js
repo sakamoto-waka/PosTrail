8b94ffbb8ae288373688c5835bd63fb8dee6ac8b
sakamoto-waka <wakanmhl@gmail.com>
2022-05-05 03:08:47 +0000

    [ready to merge]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
+import "script.js"
 
 Rails.start()
 Turbolinks.start()
