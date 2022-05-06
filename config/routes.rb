ef1aebef67b45c5c3f6edd02a3b0d9f05d48dddf
sakamoto-waka <wakanmhl@gmail.com>
2022-05-06 02:51:41 +0000

    [add] public/notifications controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :public do
+    get 'notifications/index'
+  end
+  namespace :public do
     get 'posts/index'
     get 'posts/new'
     get 'posts/show'
