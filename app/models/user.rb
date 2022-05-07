dbd17af377483bbfc0daf823b4b3f7f0d683605c
sakamoto-waka <wakanmhl@gmail.com>
2022-05-07 13:25:00 +0000

    [update] follow working

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :account_image
-
+  
+  # フォロー機能用のアソシエーション
+  has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 15 }
   validates :introduction, length: { maximum: 100 }
@@ -24,6 +29,22 @@ class User < ApplicationRecord
       account_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     account_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
+  def follower?(user)
+    followers.include?(user)
   end
 
 end
