467b31ce1396ef205c8857883d019a4f5f9fe1d4
sakamoto-waka <wakanmhl@gmail.com>
2022-05-07 10:51:40 +0000

    [update] likes#destroy refactoring

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,25 @@
 class Public::CommentsController < ApplicationController
+  
+  def create
+    @post = Post.find(params[:book_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      flash.now[:success] = "コメントを送信しました"
+      redirect_to @post
+    else
+      render @post
+    end
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+    def comment_params
+      params.require(:comment).permit(:comment)
+    end
+  
 end
