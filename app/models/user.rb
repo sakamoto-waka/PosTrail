adc38a9312d89089ebab2455a85dff3f7cb268a3
sakamoto-waka <wakanmhl@gmail.com>
2022-05-07 02:53:06 +0000

    [update] user ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 15 }
   validates :introduction, length: { maximum: 100 }
 
+  def active_for_authentication?
+    # is_deletedがfalseならtrueを返すようにする
+    super && (is_deleted == false)
+  end
+  
 end
