9b8069d44d3ab9f858e0a96e9ff2761d534ad064
sakamoto-waka <wakanmhl@gmail.com>
2022-05-07 11:32:00 +0000

    [update] comment ok ajax later

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,19 +1,23 @@
 class Public::CommentsController < ApplicationController
+  before_action :authenticate_user!
   
   def create
-    @post = Post.find(params[:book_id])
+    @post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = @post.id
     if @comment.save
       flash.now[:success] = "コメントを送信しました"
       redirect_to @post
     else
-      render @post
+      # 非同期化時に変更
+      redirect_to post_path(@post)
     end
   end
   
   def destroy
-    
+    Comment.find(params[:id]).destroy
+    @post = Post.find(params[:post_id])
+    redirect_to @post
   end
   
   private
