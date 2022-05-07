b71e432ba8fb6a261fe27bc5887f99a550dd369b
sakamoto-waka <wakanmhl@gmail.com>
2022-05-07 09:25:53 +0000

    [add] _like_btn partial + association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destr
   has_one_attached :trail_image
 
   validates :body, presence: true, length: { maximum: 200 }
@@ -7,4 +8,9 @@ class Post < ApplicationRecord
   def get_trail_image(width, height)
     trail_image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  def liked_by?(user)
+    
+  end
+  
 end
