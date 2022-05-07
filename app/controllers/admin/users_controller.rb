a0612b9ee9731e5943a27880d3c120458cac096c
sakamoto-waka <wakanmhl@gmail.com>
2022-05-07 02:44:05 +0000

    [update] admin/users able to change is_dleted

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,12 +3,14 @@ class Admin::UsersController < ApplicationController
   before_action :user_find_from_params
   
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
-  def udpate
+  def update
     if @user.update(user_params)
       redirect_to admin_user_path(@user)
       flash[:success] = "#{@user.name}さんのユーザー情報を更新しました"
@@ -16,4 +18,5 @@ class Admin::UsersController < ApplicationController
       render :edit
     end
   end
+  
 end
