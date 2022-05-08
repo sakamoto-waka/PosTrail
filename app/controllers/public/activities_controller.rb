182d80f2e826ce2d071dfdf43fd303dbe52dd563
sakamoto-waka <wakanmhl@gmail.com>
2022-05-08 00:52:49 +0000

    [update] activity ok

--- app/controllers/public/activities_controller.rb
+++ app/controllers/public/activities_controller.rb
@@ -1,4 +1,5 @@
 class Public::ActivitiesController < ApplicationController
   def index
+    @activities = current_user.active_notifications.page(params[:page]).per(20)
   end
 end
--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -5,6 +5,7 @@ class Public::NotificationsController < ApplicationController
     notifications.where(checked: false).each do |notification|
       notification.update_attribute(:checked, true)
     end
-    @notifications_except_me = notifications.where.not(visitor_id: current_user.id).page(params[:page]).per(15)
+    
+    @notifications_except_me = notifications.where.not(visitor_id: current_user.id).page(params[:page]).per(20)
   end
 end
