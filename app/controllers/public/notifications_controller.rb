79c1f26adf26327b99b815144dde1e259a9e1bcb
sakamoto-waka <wakanmhl@gmail.com>
2022-05-08 00:13:22 +0000

    [update] notifications logic

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,4 +1,10 @@
 class Public::NotificationsController < ApplicationController
   def index
+    notifications = current_user.passive_notifications
+    # 通知を一回したら確認済みに変更
+    notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+    @notifications_except_me = notifications.where.not(visitor_id: current_user.id).page(params[:page]).per(15)
   end
 end
