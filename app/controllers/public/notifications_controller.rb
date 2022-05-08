0c640ded816e9860ef635c9e2da68b69be9b3a42
sakamoto-waka <wakanmhl@gmail.com>
2022-05-08 00:33:49 +0000

    [update] notification working correctly

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -3,7 +3,7 @@ class Public::NotificationsController < ApplicationController
     notifications = current_user.passive_notifications
     # 通知を一回したら確認済みに変更
     notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+      notification.update_attribute(:checked, true)
     end
     @notifications_except_me = notifications.where.not(visitor_id: current_user.id).page(params[:page]).per(15)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -15,11 +15,11 @@ class Post < ApplicationRecord
   def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
-  
+
   # 通知用メソッド
   def create_notification_like(current_user)
     # 既にいいねされてるか検索(何度も同じ人から通知が来ないようにするため)
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ?", current_user.id, user_id, id, 'like'])
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ?", current_user.id, user_id, id, 'like'])
     # いいねされてない時のみ通知レコード作成
     if temp.blank?
       # postに付くメソッドなのでここのidはpostが持つidとなる
@@ -32,30 +32,30 @@ class Post < ApplicationRecord
       end
       notification.save if notification.valid?
     end
-    
+  end
+
     # 自分以外のコメントしている人をすべて取得→全員に通知を送る(saveはまだ)
-    def create_notification_comment(current_user, comment_id)
-      temp_ids = Comment.select(:user_id).where(post_id: id)
-                                         .where.not(user_id: current_user.id).distinct #←自分と重複を排除
-      temp_ids.each do |temp_id|
-        save_notification_comment(current_user, comment_id, temp_id[('user_id')])
-      end
-      # 初コメントなら投稿者にそのまま通知する
-      save_notification_comment(current_user, comment_id, user_id) if temp_ids.blank?
+  def create_notification_comment(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(post_id: id)
+                                       .where.not(user_id: current_user.id).distinct #←自分と重複を排除
+    temp_ids.each do |temp_id|
+      save_notification_comment(current_user, comment_id, temp_id[('user_id')])
     end
-      
-    def save_notification_comment(current_user, comment_id, visited_id)  
-      # 通知存在確認はしない→複数回コメントしてもきちんと全て通知される
-      notification = current_user.active_notifications.new(post_id: id,
-                                                           comment_id: comment_id,
-                                                           visited_id: visited_id,
-                                                           action: 'comment')
-      # 自分の投稿への自分のコメントは通知済みにする
-      if notification.visitor_id == notification.visited_id
-        notification.checked = true
-      end
-      notification.save if notification.valid?
-      
+    # 初コメントなら投稿者にそのまま通知する
+    save_notification_comment(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment(current_user, comment_id, visited_id)
+    # 通知存在確認はしない→複数回コメントしてもきちんと全て通知される
+    notification = current_user.active_notifications.new(post_id: id,
+                                                         comment_id: comment_id,
+                                                         visited_id: visited_id,
+                                                         action: 'comment')
+    # 自分の投稿への自分のコメントは通知済みにする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
   end
 
 end
