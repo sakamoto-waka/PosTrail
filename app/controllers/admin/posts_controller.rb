171f0a9afcac02ea4cfa20523f1beb5ef482f380
sakamoto-waka <wakanmhl@gmail.com>
2022-05-09 10:07:13 +0000

    [fix+update] search rusult page show tag result + layouts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,10 +3,12 @@ class Admin::PostsController < ApplicationController
   before_action :post_find_from_params, only: [:show, :destroy]
 
   def index
+    tags_list = Tag.find(PostTag.group(:tag_id).order('count(post_id) desc').limit(25).pluck(:tag_id))
+    @tags_list = Kaminari.paginate_array(tags_list).page(params[:page]).per(10)
     @posts = Post.all
   end
   
-  def tags_list
+  def tags_index
     @tags = Tag.includes(:posts).sort { |key, val| key.posts.size <=> val.posts.size }
   end
 
