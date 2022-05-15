070572bec0998d8244c00b56bde9f49332d64d1f
sakamoto-waka <wakanmhl@gmail.com>
2022-05-15 01:01:36 +0000

    [update] search ok design not yet

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,10 +1,10 @@
 class Public::SearchesController < ApplicationController
 
   def search
-    content = params[:content]
-    @posts = Post.looks(content).page(params[:page]).per(20)
-    @users = User.looks(content).where("is_deleted = ?", false).page(params[:page]).per(10)
-    tags = Tag.looks(content)
+    @content = params[:content]
+    @posts = Post.looks(@content).page(params[:page]).per(20)
+    @users = User.looks(@content).where("is_deleted = ?", false).page(params[:page]).per(10)
+    tags = Tag.looks(@content)
     @tags = Kaminari.paginate_array(tags).page(params[:page]).per(10)
   end
 
