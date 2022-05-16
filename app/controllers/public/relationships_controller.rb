24d8eecfb79e35a30948f733d2291e472fbc27b6
sakamoto-waka <wakanmhl@gmail.com>
2022-05-16 00:55:21 +0000

    [update] following page partial ok

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -15,14 +15,14 @@ class Public::RelationshipsController < ApplicationController
 
   # フォローしてる人一覧
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end
 
   # フォロワー一覧
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
   end
 
 end
