758f0cc09e21c02b5b1096ecaeb43194aae17204
sakamoto-waka <wakanmhl@gmail.com>
2022-05-17 03:43:58 +0000

    [update] folllow ajax

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -5,12 +5,13 @@ class Public::RelationshipsController < ApplicationController
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
     @user.create_notification_follow(current_user)
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   # フォロー/フォロワーの一覧
