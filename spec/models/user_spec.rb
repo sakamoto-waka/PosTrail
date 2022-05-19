77962ddde95fb69f118f449158ad0da5c9645841
sakamoto-waka <wakanmhl@gmail.com>
2022-05-19 14:25:29 +0000

    [bundle install] gem 'factory_bot_rails'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "User" do
+    it "nameとemailを持っている"
+    it "nameがないと無効になる"
+  end
+  
 end
