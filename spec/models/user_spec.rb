5b43693f8c0f7ac58cfe8b9801fd2c4b0aa6faf0
sakamoto-waka <wakanmhl@gmail.com>
2022-05-19 15:36:21 +0000

    [update] user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  describe "モデル" do
+  describe 'バリデーションのテスト' do
     before(:each) do
       @user = create(:user)
     end
       
-    it "有効な値であれば保存される" do
+    it '有効な値であれば保存される' do
       expect(@user).to be_valid
     end
     
-    it "nameとemailを持っている" 
-      
-    it "nameがないと無効になる" do
-      user = FactoryBot.build(:user, name: nil)
-      expect(user).to be_invalid
+    describe "nameカラム" do
+      it 'ないと無効になる' do
+        @user.name = ""
+        expect(@user).to be_invalid
+      end
+      it '15文字以下であること' do
+        @user.name = "a" * 16
+        expect(@user).to be_invalid
+      end
     end
   end
   
