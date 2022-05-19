9bad218ed3b89fc97cfb0583d5c50e8f22932bd3
sakamoto-waka <wakanmhl@gmail.com>
2022-05-19 15:09:03 +0000

    [fix] user model test 1 pass

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
-FactoryBot.defined do
+FactoryBot.define do
   factory :user do
     name { "aaaa" }
     email { "aaaa@aaaa.com" }
     password { "password" }
     encrypted_password { "password" }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   describe "User" do
+    it "有効な値であれば保存される" do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+    
     it "nameとemailを持っている"
+      
     it "nameがないと無効になる"
   end
   
