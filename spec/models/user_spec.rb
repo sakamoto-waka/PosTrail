1636971cff3623dfe892cb845367603d62914af4
sakamoto-waka <wakanmhl@gmail.com>
2022-05-19 15:25:46 +0000

    [add] factories/post.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  describe "User" do
+  describe "モデル" do
+    before(:each) do
+      @user = create(:user)
+    end
+      
     it "有効な値であれば保存される" do
-      expect(FactoryBot.build(:user)).to be_valid
+      expect(@user).to be_valid
     end
     
-    it "nameとemailを持っている"
+    it "nameとemailを持っている" 
       
-    it "nameがないと無効になる"
+    it "nameがないと無効になる" do
+      user = FactoryBot.build(:user, name: nil)
+      expect(user).to be_invalid
+    end
   end
   
 end
