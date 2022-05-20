4188282576821acbe74d24077e54bbaac3dc4e12
sakamoto-waka <wakanmhl@gmail.com>
2022-05-20 04:14:18 +0000

    [update] comment model rspec validation ok

---
+++ spec/factories/comment.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :comment do
+    comment { 'テスト用コメント' }
+    user
+    post
+  end
+
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'モデルに関するテスト' do
+    before do
+      @comment = build(:comment)
+    end
+    it 'commentが2文字であれば有効であること' do
+      expect(@comment).to be_valid
+    end
+    it 'commentがなければ無効であること' do
+      @comment.comment = ''
+      expect(@comment).to be_invalid
+    end
+    it 'commentが101文字であれば無効であること' do
+      @comment.comment = 'a' * 101
+      expect(@comment).to be_invalid
+    end
+  end
 end
