0bab96a2aa26390f4f5f8dd87fb6f2466832b5ed
sakamoto-waka <wakanmhl@gmail.com>
2022-05-20 14:00:53 +0000

    [add] requests/public_post_request.rb / invalid system test webdriver

--- spec/system/public_users_spec.rb
+++ spec/system/public_users_spec.rb
@@ -1,9 +1,35 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "ユーザー新規登録", type: :system do
-  before do
-    let(:user) { build(:user) }
-  end
+# RSpec.describe 'ユーザー新規登録', type: :system do
+#   let(:user) { build(:user) }
+#   describe 'ユーザー新規登録ができること' do
+#     it '正しい情報を入力すればユーザー新規登録が出来てuser_pathに移動する' do
+#       # トップページに移動
+#       visit root_path
+#       # トップページにサインアップへ遷移する新規登録ボタンがあることを確認する
+#       expect(page).to have_content('新規登録')
+#       # 新規登録ページへ移動
+#       visit new_user_registration_path
+#       # ユーザー情報を入力
+#       fill_in 'user_ユーザーネーム', with: user.name
+#       fill_in 'user_email', with: user.email
+#       fill_in 'user_password', with: user.password
+#       fill_in 'user_password_confirmation', with: user.encrypted_password
+#       # 新規登録ボタンを押すとユーザーモデルのカウントが1上がることを確認する
+#       expect{
+#         click_button('新規登録')
+#       }.to change { User.count }.by(1)
+#       # user_pathへ遷移する
+#       expect(current_path).to eq(user_path(user))
+#       # マイページの表示がある
+#       expect(page).to have_content('マイページ')
+#       # ログアウトボタンが表示されている事を確認
+#       expect(page).to have_content('ログアウト')
+#       # 新規登録ボタンやログインボタンが無いことを確認する
+#       expect(page).to_not have_content('新規登録', 'ログイン')
+#       # ユーザーの投稿のみが表示されている
+#       expect(page).to have_content(user.posts)
+#     end
+#   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
+# end
