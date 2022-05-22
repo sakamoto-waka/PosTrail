f179f4dc22a9d0b84b52d8c6a5a3e34ff8ddc5cf
sakamoto-waka <wakanmhl@gmail.com>
2022-05-22 00:59:11 +0000

    [update] login module admin_login unused yet

--- spec/support/login_module.rb
+++ spec/support/login_module.rb
@@ -5,4 +5,10 @@ module LoginModule
     fill_in 'user_password', with: user.password
     click_button('ログイン')
   end
+  def admin_login(admin)
+    visit new_admin_session_path
+    fill_in 'user_email', with: admin.email
+    fill_in 'user_password', with: admin.password
+    click_button('ログイン')
+  end
 end
