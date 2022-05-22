8934c4a3269adce8f8c3d170c87289aaeee67765
sakamoto-waka <wakanmhl@gmail.com>
2022-05-22 14:03:13 +0000

    [update] user, post model test for image valid

--- spec/system/public/likes_spec.rb
+++ spec/system/public/likes_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'いいね機能に関するテスト', type: :system do
-  let!(:user) { create(:user) }
+  let!(:other_user) { create(:user, :other_user) }
   let!(:post) { create(:post) }
   context 'ユーザーがログインをしているとき' do
     it '他人の投稿にいいねができること' do
-      login(user)
+      login(other_user)
       # 投稿一覧にいく
       visit posts_path
       # postがあることを確認
-      expect(page).to have_content('')
+      expect(page).to have_content(post.body)
       # いいねボタンを探してクリック
+      find("#like_button_#{post.id}").click
       # postのいいねが1になる
-      
+      # visit post_path(post)
+      # expect(post.likes.count).to eq(1)
       
     end
   end
