ff8d08c183f78cdf9a1a07f5eaedc5d9e914950d
sakamoto-waka <wakanmhl@gmail.com>
2022-05-22 00:34:51 +0000

    [update] like model validation for user_id + post_id presence

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,6 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
   
-  # validates
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -6,12 +6,16 @@ RSpec.describe Like, type: :model do
   let(:post) { create(:post) }
   describe 'いいね機能に関するテスト' do
     describe 'likeモデルのバリデーション' do
-      context 'user_idとpost_idがあるとき' do
-        it '保存ができること' do
-          like.user_id = like_user.id
-          like.post_id = post.id
-          expect(like).to be_valid
-        end
+      it 'user_idとpost_idがあれば保存ができること' do
+        like.user_id = like_user.id
+        like.post_id = post.id
+        expect(like).to be_valid
+      end
+      it 'user_idがなければ無効なこと' do
+        like.user_id = nil
+        like.post_id = post.id
+        expect(like).to be_invalid
+        expect(like.errors[:user_id]).to include('を入力してください')
       end
     end
   end
