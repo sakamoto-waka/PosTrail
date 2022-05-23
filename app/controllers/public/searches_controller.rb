c5d89e3d844ee8d904d49606cd619d27b224dfc6
sakamoto-waka <wakanmhl@gmail.com>
2022-05-23 12:09:56 +0000

    [update] searches controller for bullet alert

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,9 +2,9 @@ class Public::SearchesController < ApplicationController
 
   def search
     @content = params[:content]
-    @posts = Post.looks(@content).page(params[:page]).per(20)
-    @users = User.looks(@content).where("is_deleted = ?", false).page(params[:page]).per(10)
-    tags = Tag.looks(@content)
+    @posts = Post.with_attached_trail_image.includes([:tags, :user => { account_image_attachment: :blob }]).looks(@content).page(params[:page]).per(20)
+    @users = User.with_attached_account_image.looks(@content).where("is_deleted = ?", false).page(params[:page]).per(10)
+    tags = Tag.includes([:posts => { trail_image_attachment: :blob }]).looks(@content)
     @tags = Kaminari.paginate_array(tags).page(params[:page]).per(10)
   end
 
