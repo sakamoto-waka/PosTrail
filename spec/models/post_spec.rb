8414ed00a52075cf04346ec97f5907d8849fabaf
sakamoto-waka <wakanmhl@gmail.com>
2022-05-27 04:01:00 +0000

    [update] models/post_spec like notification test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let!(:post) { create(:post) }
-  let!(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:user) { create(:user) }
   describe 'モデルに関するテスト' do
     describe 'バリデーションのテスト' do
       context '成功するとき' do
@@ -45,6 +45,42 @@ RSpec.describe Post, type: :model do
         let!(:like) { create(:like, post_id: post.id) }
           it 'flaseが返ること' do
             expect(post.liked_by?(user)).to be_falsey
+        end
+      end
+      describe 'create_notification_like(current_user)のテスト' do
+        let(:other_user) { create(:user) }
+        let(:my_post) { create(:post, user_id: other_user.id) }
+        context '通知が作成されるとき' do
+          context 'userへの通知が初めてのとき' do
+            it 'userのactive_notificationsが1増えること' do
+              expect(user.active_notifications.count).to eq 0
+              expect{
+                my_post.create_notification_like(user)
+              }.to change{ user.active_notifications.count }.by 1
+            end
+          end
+          context 'other_userいいねしたとき' do
+            it 'postのuserのpassive_notificationsが1増えること' do
+              expect{
+                my_post.create_notification_like(user)
+              }.to change{ other_user.passive_notifications.count }.by 1
+            end
+          end
+          it 'userへの通知のactionがlikeなこと' do
+            my_post.create_notification_like(user)
+            # いいねしているのは自分なのでvisited_idが自分のid
+            user_notice = Notification.find_by(visitor_id: user.id)
+            expect(user_notice.action).to eq 'like'
+          end
+        end
+        context '通知が作成されないとき' do
+          context 'other_userからuserへの通知が初めてではないとき' do
+            it 'active_notificationsは1のまま増えないこと' do
+              my_post.create_notification_like(user)
+              my_post.create_notification_like(user)
+              expect(user.active_notifications.count).to eq 1
+            end
+          end
         end
       end
     end
