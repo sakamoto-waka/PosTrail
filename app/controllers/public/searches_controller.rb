16068fe638547f0a083c0703369edb122d2154e4
sakamoto-waka <wakanmhl@gmail.com>
2022-05-30 12:05:06 +0000

    [find] bug for tag search controller

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,9 @@
 class Public::SearchesController < ApplicationController
   def search
     @content = params[:content]
-    @posts = Post.includes_all.joins(:user).where(user: { is_deleted: false }).looks(@content).page(params[:page]).per(20)
-    @users = User.looks(@content).where("is_deleted = ?", false).page(params[:page]).per(10)
+    @posts = Post.includes_all.latest.joins(:user).where(user: { is_deleted: false }).looks(@content).page(params[:page]).per(25)
+    @users = User.looks(@content).where("is_deleted = ?", false).page(params[:page]).per(25)
     tags = Tag.includes([:posts => { trail_image_attachment: :blob }]).looks(@content)
-    @tags = Kaminari.paginate_array(tags).page(params[:page]).per(10)
+    @tags = Kaminari.paginate_array(tags).page(params[:page]).per(25)
   end
 end
