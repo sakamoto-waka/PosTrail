90dcb234d2e9c82a19ce9e3dfdab29ac4245ebca
sakamoto-waka <wakanmhl@gmail.com>
2022-06-02 15:43:21 +0000

    [update] question controller not done

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -1,13 +1,42 @@
 class Public::QuestionsController < ApplicationController
+  before_action :authenticate_user! || :authenticate_admin!, only: %w(create edit update)
+  
   def index
+    @questions = Question.all
+    @question = Question.new
   end
 
-  def new
+  def create
+    @question = current_user.questions(question_params)
+    if @question.save
+      redirect_to questions_path
+      flash[:success] = "質問を送信しました"
+    else
+      @questions = Question.all
+      render :new
+      flash[:danger] = "質問を送信できませんでした"
+    end
   end
 
   def show
+    @qusetion = Question.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+    
+    def ensure_correct_user
+      @qusetion = Question.find(params[:id])
+      user = User.find(params[:user_id])
+      unless @question.user == user || admin_signed_in?
+        redirect_to new_user_session_path
+        flash[:danger] = "ログインをすると質問できます"
+      end
+    end
+    
+    def question_params
+      params.require(:question).permit(:content, :answer, :riding_experience)
+    end
 end
