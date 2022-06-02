dad20c283f23c13599f5c909d5034f4979253454
sakamoto-waka <wakanmhl@gmail.com>
2022-06-02 15:27:02 +0000

    [update] question model association+validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :user
+  
+  validates :content, length: { minimum: 2, maximum: 250 }
+  validates :answer, length: { minimum: 2, maximum: 250 }
+  validates :title, length: { minimum: 2, maximum: 50 }
+  validates :riding_experience, length: { minimum: 2, maximum: 20 }
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :questions, dependent: :destroy
   has_one_attached :account_image
 
   # フォロー機能用のアソシエーション
