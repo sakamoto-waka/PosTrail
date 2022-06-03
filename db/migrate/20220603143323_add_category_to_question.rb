03a850f246921c111ff9ed02310298a6adca9f90
sakamoto-waka <wakanmhl@gmail.com>
2022-06-03 14:39:52 +0000

    [update] migration file for category column

--- db/migrate/20220603143323_add_category_to_question.rb
+++ db/migrate/20220603143323_add_category_to_question.rb
@@ -1,5 +1,11 @@
 class AddCategoryToQuestion < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :questions, :category, :integer
+    change_column :questions, :category, :integer, :null => false, default: 0
   end
+
+  def down
+    remove_column :questions, :category
+  end
+
 end
