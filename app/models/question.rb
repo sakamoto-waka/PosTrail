e152691413b87ea99d122004e0a3703a24139288
sakamoto-waka <wakanmhl@gmail.com>
2022-06-04 01:04:02 +0000

    [update] enum ok

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :user
-  
+
   validates :content, length: { minimum: 2, maximum: 250 }
   validates :answer, length: { minimum: 2, maximum: 250 }
   validates :title, length: { minimum: 2, maximum: 50 }
   validates :riding_experience, length: { minimum: 2, maximum: 20 }
-  
-  enum category: { 初心者: 0, 中級者: 1, 上級者:  }
+
+  enum category: { 初心者: 0, 中級者: 1, 上級者: 2 }
 end
