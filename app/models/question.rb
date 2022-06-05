491c0a3026a3c5d149b81eb705cacf336778f93b
sakamoto-waka <wakanmhl@gmail.com>
2022-06-05 01:14:08 +0000

    [update] question model spelling miss

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,5 +5,5 @@ class Question < ApplicationRecord
   validates :title, length: { minimum: 2, maximum: 50 }
   validates :riding_experience, length: { minimum: 2, maximum: 20 }
 
-  enum category: { biginner: 0, intermediate: 1, advanced: 2 }
+  enum category: { beginner: 0, intermediate: 1, advanced: 2 }
 end
