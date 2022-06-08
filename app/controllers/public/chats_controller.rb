f17f5e13431ddf4f53586afae2119ad2de00a235
sakamoto-waka <wakanmhl@gmail.com>
2022-06-08 02:56:55 +0000

    [fix] chats ajax ok

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -5,29 +5,31 @@ class Public::ChatsController < ApplicationController
     @user = User.find(params[:id])
     room_ids = current_user.user_rooms.pluck(:room_id)
     # ↑配列で取得されたものに↓最初にマッチしたUserRoomを取得
-    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: room_ids)
+    user_room = UserRoom.find_by(user_id: @user.id, room_id: room_ids)
 
-    if user_rooms.blank?
+    if user_room.blank?
       @room = Room.new
       @room.save
       UserRoom.create(user_id: @user.id, room_id: @room.id)
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
     else
-      @room = user_rooms.room
+      @room = user_room.room
     end
-    @chats = @room.chats
-    @chat = Chat.new(room_id: @room.id)
+    @chats = @room.chats.includes(:user)
+    @chat = Chat.new(room_id: @room.id, user_id: @user.id)
   end
 
   def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
+    user_room = UserRoom.find_by(user_id: @chat.user_id, room_id: @chat.room_id)
+    @chats = user_room.room.chats.includes(:user)
   end
 
   private
 
     def chat_params
-      params.require(:chats).permit(:message, :room_id)
+      params.require(:chat).permit(:message, :room_id)
     end
 
 end
