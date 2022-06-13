48c944f44d96d05f898f8f8183e52aa4c895b0a4
sakamoto-waka <wakanmhl@gmail.com>
2022-06-13 04:38:17 +0000

    [update] spec

--- spec/requests/admin/questions_spec.rb
+++ spec/requests/admin/questions_spec.rb
@@ -3,21 +3,7 @@ require 'rails_helper'
 RSpec.describe "Admin::Questions", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/admin/questions/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/admin/questions/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/admin/questions/edit"
+      get "/admin/questions"
       expect(response).to have_http_status(:success)
     end
   end
